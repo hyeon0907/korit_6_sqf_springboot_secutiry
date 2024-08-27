69fdec8b710b8f821a0977617ac2a59b62838be6
hyeond0907 <kimjh597@naver.com>
2024-08-27 17:40:39 +0900

    08-27

---
+++ SpringSecurityMybatis/src/main/java/com/study/SpringSecurityMybatis/exception/DeleteUserException.java
@@ -0,0 +1,7 @@
+package com.study.SpringSecurityMybatis.exception;
+
+public class DeleteUserException extends RuntimeException{
+    public DeleteUserException(String message){
+        super(message);
+    }
+}
--- SpringSecutiryMybatis/src/main/java/com/study/SpringSecutiryMybatis/repository/UserRolesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.study.SpringSecutiryMybatis.repository;
-
-import com.study.SpringSecutiryMybatis.entity.UserRoles;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserRolesMapper {
-    int save(UserRoles userRoles);
-}
